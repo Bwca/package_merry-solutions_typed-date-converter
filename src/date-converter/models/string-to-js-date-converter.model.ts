eb13dc6669e0f6a363ce4e0259999f49dc86313b
Volodymyr Yepishev <i.m.bwca@gmail.com>
2022-06-25 20:37:06 +0300

    removed more precise method

--- src/date-converter/models/string-to-js-date-converter.model.ts
+++ src/date-converter/models/string-to-js-date-converter.model.ts
@@ -1,4 +1,3 @@
 import { PrimitiveDateType } from './primitive-date-type.model';
 
-// eslint-disable-next-line no-unused-vars
 export type StringToJSDateConverter<S extends PrimitiveDateType, D> = (date: S) => D;
